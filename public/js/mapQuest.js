d747195b55cf6bbb20211a5dcdfe00c5910b66bf
Hillary <hthi@ucsd.edu>
2019-02-28 10:08:00 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'kadsyUavKQrLeAT3wyKYUXAcPV7QWX9a';
+
+	//'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map',{
+		center: [32.878852, -117.235932],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	//Add a marker to the map
+	L.marker([32.878852, -117.235932]).addTo(map);
 }
